3c1944ad113ba018c0e665d9f26376e396c6d634
Le <male111@163.com>
2024-02-26 17:32:56 +1300

    Initial Commit

---
+++ KindHeartsDemo/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  KindHeartsDemo
+//
+//  Created by 马乐 on 26/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "gwwwwwwwwwwwwlobe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("None")
+            Text("Hello, wokhkhjrwewqeweqeqweqwedsadld!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KindHeartsDemo/KindHeartsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KindHeartsDemoApp.swift
+//  KindHeartsDemo
+//
+//  Created by 马乐 on 26/02/24.
+//
+
+import SwiftUI
+
+@main
+struct KindHeartsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
